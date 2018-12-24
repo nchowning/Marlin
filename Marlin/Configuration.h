f757f442264c6afee7ffeb3716d8c9cf30284225
Nathan Chowning <nathan@chowning.me>
2018-12-23 21:30:59 -0500

    Fixed extrusion s/mm & z probe offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -612,7 +612,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 600 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 357.14 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -782,7 +782,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER 28  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER 11  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -3.2   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -0.85   // Z offset: -below +above  [the nozzle]
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 30
